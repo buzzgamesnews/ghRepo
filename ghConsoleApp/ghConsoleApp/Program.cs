364f4cac69d901ae95f94ff47d0fb8e85aa3b668
Олег <luchinskiy@rambler.ru>
2015-09-25 13:25:05 +0300

    sss

--- ghConsoleApp/ghConsoleApp/Program.cs
+++ ghConsoleApp/ghConsoleApp/Program.cs
@@ -13,8 +13,8 @@ namespace ghConsoleApp
             Console.WriteLine("Change master branch!");
             GoodBuy();
 
-            Console.Write("123");
-            Console.Write("321");
+            Console.Write("123---asd");
+            Console.Write("321===asd");
 
             Console.ReadKey();
         }

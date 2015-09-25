920b1151eafd93454d15fd6709bffab98b86d23b
Олег <luchinskiy@rambler.ru>
2015-09-25 17:45:58 +0300

    s

--- ghConsoleApp/ghConsoleApp/Program.cs
+++ ghConsoleApp/ghConsoleApp/Program.cs
@@ -13,12 +13,13 @@ namespace ghConsoleApp
             Console.WriteLine("Change master branch!");
             GoodBuy();
 
+<<<<<<< HEAD
             Console.Write("This is test branch");
 
-
+=======
             Console.Write("123---asd");
             Console.Write("321===asd");
-
+>>>>>>> 3fdc1ef... Merge branch 'Feature/Conflict'
 
             Console.ReadKey();
         }
